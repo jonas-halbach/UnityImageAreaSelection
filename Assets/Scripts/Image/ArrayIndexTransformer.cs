b0fc4b0f0c80ffd91df6b097545492fa4a9702d2
Jonas <jonas@jonas-halbach.de>
2020-02-02 20:03:23 +0100

    Added the class "ArrayIndexTransformer", which transforms the index from one array into the Index of an other array.
    The arrays on which the transformation is based are two linear array, which represent a two dimensional structure (e.g. an image).  The idea of this class is, that we want insert the content of one of the arrays inside of the other array (e.g. one of the arrays is smaller than the other). the data (Color of a pixel) will be inserted inside of the other image. For using this class the width and heigth of both images/arrays (original and target image need to be specified) to be able to execute a proper index conversion.

---
+++ Assets/Scripts/Image/ArrayIndexTransformer.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+
+namespace com.halbach.imageselection.image
+{
+    public class ArrayIndexTransformer 
+    {
+        public int TargetWidth{
+            get; set;
+        }
+
+        public int TargetHeight{
+            get; set;
+        }
+
+        public int OriginalHeight{
+            get;set;
+        }
+
+        public int OriginalWidth {
+            get;set;
+        }
+
+        private int UpperBorder {
+            get; set;
+        }
+
+        private int LeftBorder {
+            get; set;
+        }
+
+        private int RightBorder {
+            get; set;
+        }
+
+        public ArrayIndexTransformer(int originalWidth, int originalHeight, int targetWidth, int targetHeight)
+        {
+            OriginalWidth = originalWidth;
+            OriginalHeight = originalHeight;
+            TargetWidth = targetWidth;
+            TargetHeight = targetHeight;
+
+            Initialize();
+        }
+
+        private void Initialize() {
+            UpperBorder = CalculateUpperBorder();
+            LeftBorder = CalculateLeftBorder();
+            RightBorder = LeftBorder;
+        }
+
+        public int TransformTargetImageCoordinateToSourceImageIndex(int targetCol, int targetRow)
+        {
+            Vector2Int coordinateInOriginalImage = TransformTargetImageCoordinatesToOriginalCoordinates(targetCol, targetRow);
+            return CalculateOriginalImageCoordinatesToIndex(coordinateInOriginalImage);
+        }
+
+        public Vector2Int TransformTargetImageCoordinatesToOriginalCoordinates(int targetCol, int targetRow)
+        {
+            int originalCol = targetCol - LeftBorder;
+            int originalRow = targetRow - UpperBorder;
+
+            originalCol = originalCol >= OriginalWidth ? -1 : originalCol;
+            originalRow = originalRow >= OriginalHeight ? -1 : originalRow;
+
+            return new Vector2Int(originalCol, originalRow);
+        }
+
+        public int CalculateOriginalImageCoordinatesToIndex(Vector2Int colRowVector)
+        {
+            return CalculateOriginalPixelIndexFromOriginalImageRowCol(colRowVector.x, colRowVector.y);
+        }
+
+        public int CalculateOriginalPixelIndexFromOriginalImageRowCol(int col, int row)
+        {
+            return CalculateArrayIndexFromColRow(col, row, OriginalWidth, OriginalHeight);
+        }
+
+        public int CalculateTargetPixelCoordinateToIndex(int col, int row)
+        {
+            return CalculateArrayIndexFromColRow(col, row, TargetWidth, TargetHeight);
+        }
+
+        private int CalculateArrayIndexFromColRow(int col, int row, int width, int height) {
+            int index = -1;
+
+            if(col >-1 && row > -1 && col < width && row < height)
+            {
+                index = row * width + col;
+            }
+
+            return index;
+        }
+
+        public int TransformTargetImageIndexToSourceImageIndex(int indexInTargetImage, int col, int row)
+        {
+            int startingRowOriginalImage = row - UpperBorder;
+            int sideBorder = (LeftBorder + RightBorder);
+            int pixelsOutOfOriginalImageBounds = startingRowOriginalImage * sideBorder;
+            return indexInTargetImage - TargetWidth * UpperBorder - LeftBorder + pixelsOutOfOriginalImageBounds;
+        }
+
+        private int CalculateLeftBorder()
+        {
+            return  CalculateBorder(OriginalWidth,  TargetWidth);
+        }
+
+        private int CalculateUpperBorder(){
+            return CalculateBorder(OriginalHeight, TargetHeight);
+        }
+
+        private int CalculateBorder(int originalImageSize, int targetImageSize)
+        {
+            return Mathf.Max((targetImageSize - originalImageSize) / 2, 0);
+        }
+    }
+}
