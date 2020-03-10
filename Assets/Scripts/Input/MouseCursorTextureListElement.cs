a950f6cad608a10631528b3ed8da2221856042ea
Jonas <jonas@jonas-halbach.de>
2020-03-11 00:42:27 +0100

    Added a MouseCursorTextureListElement to the project, which is a container to associate a TargetMousePosition to a Texture2D (MouseCursor Image)

---
+++ Assets/Scripts/Input/MouseCursorTextureListElement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace com.halbach.imageselection.input {
+    
+    [Serializable]
+    public class MouseCursorTextureListElement
+    {
+        [SerializeField]
+        private TargetMousePosition key;
+
+        [SerializeField]
+        private Texture2D value;
+
+        public TargetMousePosition Key {
+            get
+            {
+                return key;
+            }
+            set
+            {
+                key = value;
+            }
+        }
+
+        public Texture2D Value {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public MouseCursorTextureListElement(TargetMousePosition key, Texture2D value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+    }
+}
