200bf72b96d28f518314109c30ef27e48217eff3
Jonas <jonas@jonas-halbach.de>
2020-03-20 00:18:37 +0100

    Refactoring: Added a MouseDragState, which shall combine the functionality for the MouseStates which change the selection rectangle by dragging the mouse

---
+++ Assets/Scripts/Input/MouseInputStates/MouseDragState.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+namespace com.halbach.imageselection.input {
+    public abstract class MouseDragState : MouseInputState 
+    {
+        private bool mouseDown;
+
+        private float movingSpeed = 0.02f;
+
+        private Vector3 lastMousePosition;
+
+        public MouseDragState(TargetMousePosition mouseCorner, MousePropertyContainer mousePropertyContainer, RectTransform transformTarget) 
+                                        : base(mouseCorner, mousePropertyContainer, transformTarget)
+        {
+            mouseDown = false;
+        }
+
+        public override IMouseInputState MouseDown(Vector2 mousePosition)
+        {
+            Debug.LogWarning("MouseDown " + this.GetType().ToString());
+
+            lastMousePosition = mousePosition;
+            mouseDown = true;
+            return this;
+        }
+
+        public override IMouseInputState MouseUp(Vector2 mousePosition)
+        {
+            lastMousePosition = mousePosition;
+            mouseDown = false;
+            return this;
+        }
+
+        public override IMouseInputState UpdateMousePostion(Vector2 mousePosition)
+        {   
+            currentMousePosition = mousePosition;
+
+            return OnMouseMove(mousePosition);
+        }
+
+        private IMouseInputState OnMouseMove(Vector2 mousePosition)
+        {
+            IMouseInputState newState = this;
+
+            Debug.LogWarning("OnMouseMove " + this.GetType().ToString());
+
+            if(mouseDown)
+            {
+                Vector3 delta = CalculateChangeVector(mousePosition);
+
+                UpdateSelectionIndicatorPosition(delta);
+            }
+            else
+            {
+                newState = GetMouseOverState(mousePosition);
+            }
+            lastMousePosition = currentMousePosition;
+
+            return newState;
+        }
+
+        private Vector3 CalculateChangeVector(Vector3 mousePosition)
+        {
+            Vector3 delta = CalculateMouseMovementDelta(mousePosition);
+            delta *= movingSpeed;
+            return delta;
+        }
+
+        private Vector3 CalculateMouseMovementDelta(Vector3 newMoousePostion)
+        {
+            Vector3 currentMousePosition = newMoousePostion;
+            Vector3 delta = lastMousePosition - currentMousePosition;
+
+            return delta;
+        }
+
+        protected abstract void UpdateSelectionIndicatorPosition(Vector3 delta);
+    }
+}
--- Assets/Scripts/Input/MouseInputStates/MouseOverRectState.cs
+++ Assets/Scripts/Input/MouseInputStates/MouseOverRectState.cs
@@ -2,78 +2,16 @@ using UnityEngine;
 
 namespace com.halbach.imageselection.input {
     
-    public class MouseOverRectState : MouseInputState
+    public class MouseOverRectState : MouseDragState
     {
         private Texture2D mouseOverRectTexture;
-
-        private Vector3 lastMousePosition;
-
-        private bool mouseDown;
-
-        private float movingSpeed = 0.02f;
 
         public MouseOverRectState(TargetMousePosition mouseCorner, MousePropertyContainer mousePropertyContainer, RectTransform transformTarget) 
                                         : base(mouseCorner, mousePropertyContainer, transformTarget)
         {
-            mouseDown = false;
         }
 
-        public override IMouseInputState UpdateMousePostion(Vector2 mousePosition)
-        {   
-            currentMousePosition = mousePosition;
-
-            return OnMouseMove(mousePosition);
-        }
-
-        public override IMouseInputState MouseDown(Vector2 mousePosition)
-        {
-            lastMousePosition = mousePosition;
-            mouseDown = true;
-            return this;
-        }
-
-        public override IMouseInputState MouseUp(Vector2 mousePosition)
-        {
-            lastMousePosition = mousePosition;
-            mouseDown = false;
-            return this;
-        }
-
-        private IMouseInputState OnMouseMove(Vector2 mousePosition)
-        {
-            IMouseInputState newState = this;
-
-            if(mouseDown)
-            {
-                Vector3 delta = CalculateChangeVector(mousePosition);
-
-                UpdateSelectionIndicatorPosition(delta);
-            }
-            else
-            {
-                newState = GetMouseOverState(mousePosition);
-            }
-            lastMousePosition = currentMousePosition;
-
-            return newState;
-        }
-
-        private Vector3 CalculateChangeVector(Vector3 mousePosition)
-        {
-            Vector3 delta = CalculateMouseMovementDelta(mousePosition);
-            delta *= movingSpeed;
-            return delta;
-        }
-
-        private Vector3 CalculateMouseMovementDelta(Vector3 newMoousePostion)
-        {
-            Vector3 currentMousePosition = newMoousePostion;
-            Vector3 delta = lastMousePosition - currentMousePosition;
-
-            return delta;
-        }
-
-        private void UpdateSelectionIndicatorPosition(Vector3 delta)
+        protected override void UpdateSelectionIndicatorPosition(Vector3 delta)
         {
             Vector3 oldRectPosition = transformTarget.position;
             oldRectPosition.x -= delta.x;
