b6cc8f2dbc591488eed63209ed97728665c327ed
Jonas <jonas@jonas-halbach.de>
2020-03-28 00:56:15 +0100

    SelectionArea will be resized in x direction if mouse is over one corner and moved when mousebutton is down

--- Assets/Scripts/Input/MouseInputStates/MouseOverCornerState.cs
+++ Assets/Scripts/Input/MouseInputStates/MouseOverCornerState.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 using System;
 
 namespace com.halbach.imageselection.input {
-    public class MouseOverCornerState : MouseInputState
+    public class MouseOverCornerState : MouseDragState
     {
         public MouseOverCornerState(TargetMousePosition mouseCorner, MousePropertyContainer mousePropertyContainer, RectTransform transformTarget) 
                                         : base(mouseCorner, mousePropertyContainer, transformTarget)
         {
+        }
+
+        protected override void UpdateSelection(Vector3 delta)
+        {
+            transformTarget.sizeDelta = new Vector2(transformTarget.sizeDelta.x - delta.x, transformTarget.sizeDelta.y);
+
+            UpdateSelection();
         }
     }
 }
