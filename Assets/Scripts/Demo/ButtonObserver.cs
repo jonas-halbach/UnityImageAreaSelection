878bce653d0a0465ca6f9148043159d09d2bf7bf
Jonas <jonas@jonas-halbach.de>
2020-05-14 00:02:38 +0200

    Added a ButtonObserver who reacts to ButtonClicks on the buttons in the DemoScene

---
+++ Assets/Scripts/Demo/ButtonObserver.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+public class ButtonObserver : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D defaultTexture;
+
+    MeshRenderer renderer;
+
+    private WebCamTexture webCam;
+
+    void Start() {
+        renderer = GetComponent<MeshRenderer>();
+        webCam = new WebCamTexture(WebCamTexture.devices[0].name);
+    }
+
+    public void OnActivateCameraButtonClick() {
+        webCam.Play();
+        renderer.material.mainTexture = webCam;
+    }
+
+    public void OnResetToDeaultButtonClick() {
+        webCam.Stop();
+        renderer.material.mainTexture = defaultTexture;
+
+    }
+}
