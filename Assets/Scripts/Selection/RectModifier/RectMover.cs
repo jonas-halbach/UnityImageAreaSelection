19db8979bd5b871f43e34aeea3fd0616c3218b25
Jonas <jonas@jonas-halbach.de>
2020-06-27 14:50:23 +0200

    Added a class which shall be used by mobile as well as mouseInput to move the selection rect

---
+++ Assets/Scripts/Selection/RectModifier/RectMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace com.halbach.imageselection.selection.RectModifier
+{
+    public class RectMover
+    {
+        protected RectTransform transformTarget;
+        public RectMover(RectTransform transformTarget) {
+            this.transformTarget = transformTarget;
+        }
+
+        public void Move(Vector3 delta) { 
+                Vector3 oldRectPosition = transformTarget.position;
+                oldRectPosition.x -= delta.x;
+                oldRectPosition.y -= delta.y;
+                transformTarget.position = oldRectPosition;
+        }
+    }
+}
