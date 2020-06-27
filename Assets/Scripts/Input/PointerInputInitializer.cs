20321528ce040f0d24c9f930d3d2dbc6e9531ab5
Jonas <jonas@jonas-halbach.de>
2020-06-27 16:00:45 +0200

    Added a class which is reponsible for instantiating mobile or mouse input depending on the plattform, where this project/asset is executed

---
+++ Assets/Scripts/Input/PointerInputInitializer.cs
@@ -0,0 +1,66 @@
+﻿using com.halbach.imageselection.selection;
+using UnityEngine;
+
+namespace com.halbach.imageselection.input {
+    public class PointerInputInitializer : MonoBehaviour
+    {
+        [SerializeField]
+        RectTransform transformTarget;
+        
+        [SerializeField]
+        BoxCollider2D selectionRectCollider;
+        
+        [SerializeField]
+        Camera renderCamera;
+
+        [SerializeField]
+        SelectionInteractionManager interactionManager;
+
+        private PointerInput input;
+
+        void Start()
+        {
+            Initialize();
+        }
+
+        void Initialize() {
+            InitializeInput();
+            InitializeInteractionManager();
+            InitializeInteractionManagerEvents();
+        }
+
+        void InitializeInput() {
+            bool isMobilePlatform = Application.isMobilePlatform;
+            if(isMobilePlatform) {
+                AddMobileInput();
+            } else {
+                AddMouseInput();
+            }
+            input.Initialize(transformTarget, selectionRectCollider, renderCamera);
+        }
+
+        void InitializeInteractionManager() {
+            if(interactionManager == null) {
+                interactionManager = gameObject.GetComponent<SelectionInteractionManager>();
+                if(interactionManager == null)
+                {
+                    interactionManager = gameObject.AddComponent<SelectionInteractionManager>();
+                    interactionManager.PreviewCamera = renderCamera;
+                    interactionManager.RectTransform = transformTarget;
+                }
+            }
+        }
+
+        void InitializeInteractionManagerEvents() {
+            interactionManager.InitializeEvents();
+        }
+
+        void AddMouseInput() {
+            input = gameObject.AddComponent<MouseInput>();
+        }
+
+        void AddMobileInput() {
+            input = gameObject.AddComponent<MobileInput>();
+        }
+    }
+}
