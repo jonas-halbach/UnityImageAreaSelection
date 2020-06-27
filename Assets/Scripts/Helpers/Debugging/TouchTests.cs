e3f7e3d6c19fb6247227c19929992ae763039af3
Jonas <jonas@jonas-halbach.de>
2020-06-27 15:00:05 +0200

    Added scripts which help debugging: These script can print the touchCount as well as additional custom messages on the screen

---
+++ Assets/Scripts/Helpers/Debugging/TouchTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTests : MonoBehaviour
+{   
+    [SerializeField]
+    private bool showDebugLog = false;
+
+    protected int touchCount = 0;
+
+    static GUIStyle style;
+    // Start is called before the first frame update
+
+    public static string debugString = string.Empty;
+
+    public static List<string> debugInfo;
+
+    void Start()
+    {
+        style = new GUIStyle();
+        style.fontSize = 22;
+        style.normal.textColor = Color.black;
+
+        debugInfo = new List<string>();
+    }
+
+    void OnGUI()
+    {
+        if(showDebugLog) {
+            GUI.Label(new Rect(10, 10, 150, 100), "TouchCount: " + touchCount, style);
+            PrintDebugText();
+        }
+        
+    }
+
+    public static void SetDebugString(string debugText) {
+        debugInfo = new List<string>();
+        debugInfo.Add(debugText);
+    }
+
+    public static void AddDebugString(string debugText) {
+        debugInfo.Add(debugText);
+    }
+
+    public static void PrintDebugText() {
+        for(int i = 0; i < debugInfo.Count; i++) {
+            string debugString = debugInfo[i];
+            GUI.Label(new Rect(10, 30 + i * 20, 150, 100), debugString, style);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Helpers/Debugging/TouchTestsInheritet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace com.halbach.helper.debugging {
+    public class TouchTestsInheritet : TouchTests
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            touchCount = Input.touchCount;
+        }
+
+    }
+}
